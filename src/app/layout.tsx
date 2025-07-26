d39bb5b81c093c6c835af3bb05d15b0f6ee56424
Francisco Borges <franciscomsborges2002@gmail.com>
2025-07-26 02:07:13 +0100

    chore: added link to analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Header } from '@/components/HeaderComponent'
 import { Footer } from '@/components/FooterComponent'
 import { ThemeProvider } from '@/components/ThemeProvider'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,6 +33,8 @@ export default function RootLayout({
           <main>
             {children}
           </main>
+
+          <Script defer src="https://umami.fborges.dev/script.js" data-website-id="7cfe6ec7-2f53-4c2c-94ef-6fd51374a8a3" />
 
           <Footer />
         </ThemeProvider>
